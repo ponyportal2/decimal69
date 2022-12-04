031056dcfc1cfa1aadcc659353f5b78995d5fc2d
Shark Meridith <sharkmer@ob-e9.nsk.21-school.ru>
2022-12-04 19:35:22 +0700

    sharkmer: decimal aboba update

--- src/s21_decimal.c
+++ src/s21_decimal.c
@@ -1,7 +1,6 @@
 #include "s21_decimal.h"
 
 #include <math.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,8 +11,9 @@
 int main() {
   // char a[AB_SIZE] = "8326731548612467647832";
   // char b[AB_SIZE] = "8326731548612467647833";
-  char a[AB_SIZE] = "87584758623475897345878784953789";
-  char b[AB_SIZE] = "4623764782364762374";
+
+  char a[AB_SIZE] = "        875.84758623475897345878784953789";
+  char b[AB_SIZE] = "46237647823.64762374000000000000000000000";
   // char a[AB_SIZE] = "9";
   // char b[AB_SIZE] = "9";
   reverse(a);
@@ -27,22 +27,28 @@ int main() {
     result[i] = calloc(RAZR_SIZE, sizeof(char));
   }
 
-  decimAddFirstStep(a, b, result);
-  decimAddSecondStep(result);
+  decimFirstStep(a, b, result, true);
+  decimSecondStepAdd(result);
   int resultSize = sizeDetector(result);
   reverseCharMatrix(result, resultSize);
   printer(result);
 
-  // strcpy(a, "87584758623475897345878784953789");
-  // strcpy(b, "4623764782364762374");
-  // reverse(a);
-  // reverse(b);
+  strcpy(a, "87584758623475897345878784953789");
+  strcpy(b, "4623764782364762374");
+  reverse(a);
+  reverse(b);
 
-  // decimSubFirstStep(a, b, result);
-  // decimSubSecondStep(result);
-  // resultSize = sizeDetector(result);
-  // reverseCharMatrix(result, resultSize);
-  // printer(result);
+  char** resultSub = calloc(RES_SIZE, sizeof(char*));
+  for (int i = 0; i < RES_SIZE; i++) {
+    resultSub[i] = calloc(RAZR_SIZE, sizeof(char));
+  }
+
+  decimFirstStep(a, b, resultSub, false);
+  printer(resultSub);
+  decimSecondStepSub(resultSub);
+  resultSize = sizeDetector(resultSub);
+  reverseCharMatrix(resultSub, resultSize);
+  printer(resultSub);
 
   // FREEING:
   // --------
@@ -54,12 +60,13 @@ int main() {
   return 0;
 }
 
-void decimAddFirstStep(char* a, char* b, char** result) {
+void decimFirstStep(char* a, char* b, char** result, bool isAddition) {
   bool aEnded = false;
   bool bEnded = false;
   int i = 0;
   int j = 0;
   char* tempForItoa = calloc(1024, sizeof(char));
+  // char tempForItoa[1024] = {0};
   bool whileBreak = false;
   while (whileBreak == false) {
     // printf("a:[%c], b:[%c]\n", a[i], b[i]);
@@ -68,9 +75,10 @@ void decimAddFirstStep(char* a, char* b, char** result) {
     if (aEnded == true && bEnded == true) {
       whileBreak = true;
     }
-    sint current = 0;
+    int current = 0;
     if (aEnded != true && bEnded != true) {
-      current = (a[i] - '0') + (b[i] - '0');
+      if (isAddition) current = (a[i] - '0') + (b[i] - '0');
+      if (isAddition == false) current = (a[i] - '0') - (b[i] - '0');
       place(current, result, tempForItoa, j);
       // printf("[%s]\n", get(result, j));
     } else if (aEnded) {
@@ -86,7 +94,7 @@ void decimAddFirstStep(char* a, char* b, char** result) {
   if (tempForItoa) free(tempForItoa);
 }
 
-void decimAddSecondStep(char** result) {
+void decimSecondStepAdd(char** result) {
   int i = 0;
   char* tempForItoa1 = calloc(1024, sizeof(char));
   // char* tempForItoa2 = calloc(1024, sizeof(char));
@@ -94,7 +102,7 @@ void decimAddSecondStep(char** result) {
     if (atoi(result[i]) > 9) {
       // place(atoi(result[i]), result, tempForItoa1, i);
       if (result[i + 1][0] != '\0') {
-        itoa(atoi(result[i + 1]) + 1, tempForItoa1, 10);
+        itoa(atoi(result[i + 1]) + 1, tempForItoa1);
         strcpy(result[i + 1], tempForItoa1);
       } else {
         result[i + 1][0] = '1';
@@ -106,7 +114,7 @@ void decimAddSecondStep(char** result) {
 
     i++;
   }
-  if (tempForItoa1) free(tempForItoa1);
+  // if (tempForItoa1) free(tempForItoa1);
   // if (tempForItoa2) free(tempForItoa2);
 }
 
@@ -115,24 +123,39 @@ void minusTen(char* input) {
   input[1] = '\0';
 }
 
-void place(sint current, char** result, char* tempForItoa, int j) {
-  itoa((int)current, tempForItoa, 10);
-  strcpy(get(result, j), tempForItoa);
+void place(int current, char** result, char* tempForItoa, int j) {
+  // printf("[%i]", current);
+  if (current < 0) {
+    itoa(current, tempForItoa);
+    // printf("! %s\n", tempForItoa);
+  } else {
+    itoa(current, tempForItoa);
+    // printf("* %s\n", tempForItoa);
+  }
+  if (strcmp(tempForItoa, "-48") == 0) {
+    strcpy(result[j], "\0");
+  } else {
+    strcpy(result[j], tempForItoa);
+  }
+
+  printf("[%s]\n", result[j]);
 }
 
-void itoa(long long num, char* src, int radix) {
+void itoa(long long num, char* src) {
   int i = 0, j = 0;
   int digit;
-  if (num < 0) {
-    src[0] = '-';
-    j++;
-  }
-  while (num >= pow(radix, i)) {
-    digit = (int)(num / pow(radix, i)) % radix;
-    src[i++] =
+  int radix;
+  radix = 10;
+  while ((num >= pow(radix, i) && num > 0) ||
+         (num <= -pow(radix, i) && num < 0)) {
+    digit = (int)(num / pow(radix, i++)) % radix;
+    if (num < 0) digit = -digit;
+    src[j++] =
         (digit >= 0 && digit <= 9) ? (char)(digit + 48) : (char)(digit + 65);
   }
-  src[i] = '\0';
+  if (num == 0) src[j++] = 48;
+  if (num < 0) src[j++] = '-';
+  src[j] = '\0';
   reverse(src);
 }
 
@@ -179,7 +202,7 @@ void reverseString(char* inputArr) {
 
 int sizeDetector(char** result) {
   int i = 0;
-  while (strlen(result[i]) > 0) {
+  while (s21_strlen(result[i]) > 0) {
     // printf("i:[%i]", i);
     // printf("s:[%s]\n", result[i]);
     i++;
@@ -189,11 +212,44 @@ int sizeDetector(char** result) {
 
 void printer(char** result) {
   int i = 0;
+  printf("out: [");
+  do {
+    if (i == 0) {
+      printf("%s", result[i]);
+    } else {
+      printf(",%s", result[i]);
+    }
+    i++;
+  } while (s21_strlen(result[i]) > 0);
+  printf("]\n");
+}
+
+size_t s21_strlen(const char* str) {
+  size_t len = 0;
+  while (*str != '\0') {
+    str++;
+    len++;
+  }
+  return (size_t)len;
+}
+
+void decimSecondStepSub(char** result) {
+  int i = 0;
+  char* tempForItoa = calloc(1024, sizeof(char));
   while (strlen(result[i]) > 0) {
-    printf("[%s]", result[i]);
+    // printf("\nDEBUG\n");
+    if (atoi(result[i]) < 0) {
+      // printf("\nDEBUG\n");
+      if (result[i + 1][0] != '\0') {
+        itoa(atoi(result[i + 1]) - 1, tempForItoa);
+        strcpy(result[i + 1], tempForItoa);
+        itoa(10 + atoi(result[i]), tempForItoa);
+        strcpy(result[i], tempForItoa);
+      }
+    }
     i++;
   }
-  printf("\n");
+  if (tempForItoa) free(tempForItoa);
 }
 
 // HOMYACHU:
@@ -230,24 +286,6 @@ void printer(char** result) {
 //     i++;
 //   }
 //   if (tempForItoa) free(tempForItoa);
-// }
-
-// void decimSubSecondStep(char** result) {
-//   int i = 0;
-//   char* tempForItoa1 = calloc(1024, sizeof(char));
-//   char* tempForItoa2 = calloc(1024, sizeof(char));
-//   while (strlen(get(result, i)) > 0) {
-//     if (atoi(result[i]) > 9) {
-//       itoa(atoi(result[i + 1]) + 1, tempForItoa1, 10);
-//       strcpy(result[i + 1], tempForItoa1);
-//       minusTen(result[i]);
-//       // itoa(atoi(result[i]) - 10, tempForItoa2, 10);
-//       // strcpy(result[i], tempForItoa2);
-//     }
-//     i++;
-//   }
-//   if (tempForItoa1) free(tempForItoa1);
-//   if (tempForItoa2) free(tempForItoa2);
 // }
 
 // printf("[%s]\n", get(result, j));
--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -1,17 +1,20 @@
 #define RAZR_SIZE 512
 #define MAXLINE_FOR_REVERSE 2048
-#define sint char
+// #define sint char
+#include <stdbool.h>
+#include <stddef.h>
 
+size_t s21_strlen(const char* str);
 void minusTen(char* input);
 void reverseString(char* inputArr);
-void decimAddFirstStep(char* a, char* b, char** result);
-void decimAddSecondStep(char** result);
+void decimFirstStep(char* a, char* b, char** result, bool isSubtraction);
+void decimSecondStepAdd(char** result);
 void reverse(char* string);
-void itoa(long long num, char* src, int radix);
+void itoa(long long num, char* src);
 char* get(char** input, int i);
 void printer(char** result);
 int sizeDetector(char** result);
 void reverseCharMatrix(char** inputArr, int n);
-void place(sint current, char** result, char* tempForItoa, int j);
+void place(int current, char** result, char* tempForItoa, int j);
 void decimSubFirstStep(char* a, char* b, char** result);
-void decimSubSecondStep(char** result);
+void decimSecondStepSub(char** result);
